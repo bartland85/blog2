c6bfe5af41b1505bcd732d842a301d3723ab0fc6
Bart <bartland85@o2.pl>
2015-05-19 22:42:28 +0200

    made index file

--- index.php
+++ index.php
@@ -4,4 +4,18 @@
  * User: Bart
  * Date: 19.05.15
  * Time: 22:26
- */ 
+ */
+
+require_once('vendor/autoload.php');
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/', function()use($app){
+
+
+    return 'ok';
+});
+
+$app->run();
